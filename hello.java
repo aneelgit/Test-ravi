adf632ca78669ec3bf97e91e3d117f18e967a92e
aneelgit <85684224+aneelgit@users.noreply.github.com>
2021-06-14 23:10:55 +0530

    copied code

--- hello.java
+++ hello.java
@@ -1,8 +1,5 @@
-class HelloWorld
-{
-	public static void main(string[] args)
-	{
-		system.out.println("welcome");
-	}	
-
-}	
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
